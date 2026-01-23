9abce2b41427f769972086d8cbae0f4e19aba752
Davies Okpeta <davies19okpeta@gmail.com>
2026-01-23 12:44:28 +0100

    Update Nuxt.js configuration to include Tailwind CSS and dev server settings; modify app.vue to display a simple "Hello world!" message.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,17 @@
+import tailwindcss from "@tailwindcss/vite";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/image', '@nuxt/scripts']
+  devServer: {
+    host: "0.0.0.0", // Listen on all network interfaces
+    port: parseInt(process.env.PORT || '8500'),
+  },
+  modules: ['@nuxt/image', '@nuxt/scripts'],
+  css: ['./app/assets/css/variables.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 })
